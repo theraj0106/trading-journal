a6e953015530e75eec42913549c79f5253898a6f
theraj0106 <theraj91@gmail.com>
2026-01-22 12:20:25 +0530

    Add service worker for caching resources
    sw.js file added

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "trading-journal-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
